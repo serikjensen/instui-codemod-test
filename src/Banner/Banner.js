b21ec175f5ec885be2c727143682089a56560a5e
Steve Jensen <sejensen@instructure.com>
2020-02-06 14:41:07 -0600

    integrate deprecated components into new codemod test

--- src/Banner/Banner.js
+++ src/Banner/Banner.js
@@ -31,6 +31,19 @@ import Text from '@instructure/ui-elements/lib/components/Text'
 import View from '@instructure/ui-layout/lib/components/View'
 import IconCheckMarkSolid from '@instructure/ui-icons/lib/Solid/IconCheckMark'
 
+import debounce from '@instructure/ui-utils/lib/debounce'
+import CustomPropTypes from '@instructure/ui-utils/lib/react/CustomPropTypes'
+
+import deprecatedComponents from '../deprecatedComponents'
+import Dialog from '../deprecatedPropNameExamples/DeprecatedDialog'
+import Example from '../Example'
+import Mask from '../deprecatedPropNameExamples/DeprecatedMask'
+import MenuItem from '../deprecatedPropNameExamples/DeprecatedMenuItem'
+import Modal from '../deprecatedPropNameExamples/DeprecatedModal'
+import Popover from '../deprecatedPropNameExamples/DeprecatedPopover'
+// import SVGIcon from '.deprecatedPropNameExamples/DeprecatedSVGIcon'
+import Tray from '../deprecatedPropNameExamples/DeprecatedTray'
+
 import Panda from './Panda'
 
 import styles from './styles.css'
@@ -69,6 +82,36 @@ class Banner extends Component {
             <Text size="large">Just edit <Text weight="bold" size="large">App.js</Text> to start building with Instructure UI.</Text>
           </View>
         </div>
+        <View
+          display="block"
+          padding="medium"
+        >
+          <Dialog />
+          <Mask />
+          <MenuItem />
+          <Modal />
+          <Popover />
+          <Tray />
+
+          <Heading
+            level="h1"
+            as="h1"
+            margin="0 0 medium 0"
+          >
+            Components
+        </Heading>
+          {deprecatedComponents.map(({ Component, props, origin }) => {
+            return (
+              <Example
+                key={`${Component.displayName}--${origin}`}
+                displayName={Component.displayName}
+                origin={origin}
+              >
+                <Component {...(props || {})} />
+              </Example>
+            )
+          })}
+        </View>
       </View>
     )
   }
---
+++ src/Example.js
@@ -0,0 +1,83 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2015 - present Instructure, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import Text from '@instructure/ui-elements/lib/components/Text'
+import View from '@instructure/ui-layout/lib/components/View'
+import Flex, { FlexItem } from '@instructure/ui-layout/lib/components/Flex'
+
+export default class Example extends React.Component {
+  static propTypes = {
+    displayName: PropTypes.string.isRequired,
+    children: PropTypes.node
+  }
+
+  static defaultProps = {
+    children: null
+  }
+
+  /* eslint-disable react/prop-types */
+  render() {
+    return (
+      <View
+        display="block"
+        margin="0 0 large 0"
+        shadow="above"
+        background="default"
+      >
+        <View
+          display="block"
+          borderWidth="0 0 small 0"
+          textAlign="start"
+          padding="small"
+          background="inverse"
+        >
+          <Flex justifyItems="space-between">
+            <FlexItem>
+              {this.props.displayName}
+            </FlexItem>
+            <FlexItem>
+              <Text fontStyle="italic">
+                {this.props.origin}
+              </Text>
+            </FlexItem>
+          </Flex>
+        </View>
+        <View
+          display="block"
+          padding="medium small medium small"
+        >
+          {/* Make this a positioned parent */}
+          <span style={{ position: 'relative' }}>
+            {this.props.children}
+          </span>
+        </View>
+      </View>
+    )
+  }
+  /* eslint-enable react/prop-types */
+}
