4da1f17bdc762d3e73cf8b67726d5cf2bf49ec7f
Steve Jensen <sejensen@instructure.com>
2020-04-03 13:47:53 -0500

    Adding export between class and decorator

--- src/Example.js
+++ src/Example.js
@@ -22,14 +22,15 @@
  * SOFTWARE.
  */
 
-
 import React from 'react'
 import PropTypes from 'prop-types'
 
-import Text from '@instructure/ui-elements/lib/components/Text'
-import View from '@instructure/ui-layout/lib/components/View'
-import Flex, { FlexItem } from '@instructure/ui-layout/lib/components/Flex'
+import themeable from '@instructure/ui-themeable/lib/themeable'
+import { Text } from '@instructure/ui-text'
+import { Flex, FlexItem } from '@instructure/ui-flex'
+import { View } from '@instructure/ui-view'
 
+@themeable()
 export default class Example extends React.Component {
   static propTypes = {
     displayName: PropTypes.string.isRequired,
@@ -47,34 +48,25 @@ export default class Example extends React.Component {
         display="block"
         margin="0 0 large 0"
         shadow="above"
-        background="default"
+        background="primary"
       >
         <View
           display="block"
           borderWidth="0 0 small 0"
           textAlign="start"
           padding="small"
-          background="inverse"
+          background="primary-inverse"
         >
           <Flex justifyItems="space-between">
+            <FlexItem>{this.props.displayName}</FlexItem>
             <FlexItem>
-              {this.props.displayName}
-            </FlexItem>
-            <FlexItem>
-              <Text fontStyle="italic">
-                {this.props.origin}
-              </Text>
+              <Text fontStyle="italic">{this.props.origin}</Text>
             </FlexItem>
           </Flex>
         </View>
-        <View
-          display="block"
-          padding="medium small medium small"
-        >
+        <View display="block" padding="medium small medium small">
           {/* Make this a positioned parent */}
-          <span style={{ position: 'relative' }}>
-            {this.props.children}
-          </span>
+          <span style={{ position: 'relative' }}>{this.props.children}</span>
         </View>
       </View>
     )
